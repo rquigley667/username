6c5ac024454e946e11d8ad9d2ec29550b5de8d56
Reese Quigley <69409681+rquigley667@users.noreply.github.com>
2020-11-15 18:47:54 -0800

    update username.sh

--- username.sh
+++ username.sh
@@ -7,10 +7,10 @@ echo "It must start with a lower case character"
 echo "It must contain at least 3 but not more than 12 characters"
 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^'[:lower:]'[a-z]''[_]'$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username code - Lower Case ans 3-12 characters!"
+	echo "Enter a vaild username: "
 	read USERNAME
 done
 echo "Thank you"
